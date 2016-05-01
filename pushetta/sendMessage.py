b5228b49d2c3871bdffb5f8a0e83e7f73b760e69
Adam Olszewski <adam@thickman.local>
2016-05-01 10:18:12 +0200

    initial commit - working logitec webcam

---
+++ pushetta/sendMessage.py
@@ -0,0 +1,6 @@
+from pushetta import Pushetta
+                  
+API_KEY="af2f35e1505f01a9a0a4ede3ee71e8024c970544"
+CHANNEL_NAME="motion channel"
+p=Pushetta(API_KEY)
+p.pushMessage(CHANNEL_NAME, "Ksiaze sie budzi :)")
