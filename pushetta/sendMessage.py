79b569da29452ba9c25582de8bc7693573637252
Adam Olszewski <adam@thickman.local>
2016-05-31 13:56:24 +0200

    pass webcam address to pushetta

--- pushetta/sendMessage.py
+++ pushetta/sendMessage.py
@@ -1,6 +1,6 @@
 from pushetta import Pushetta
-                  
+
 API_KEY="af2f35e1505f01a9a0a4ede3ee71e8024c970544"
 CHANNEL_NAME="motion channel"
 p=Pushetta(API_KEY)
-p.pushMessage(CHANNEL_NAME, "Ksiaze sie budzi :)")
+p.pushMessage(CHANNEL_NAME, "http://192.168.1.106:8081/")
