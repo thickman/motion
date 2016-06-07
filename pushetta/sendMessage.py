ff36ddb23ccd63fbf79558719e4cf151d342f964
Adam Olszewski <adam@thickman.local>
2016-06-07 13:46:13 +0200

    change ip to hostname after installing avahi deamon

--- pushetta/sendMessage.py
+++ pushetta/sendMessage.py
@@ -3,4 +3,4 @@ from pushetta import Pushetta
 API_KEY="af2f35e1505f01a9a0a4ede3ee71e8024c970544"
 CHANNEL_NAME="motion channel"
 p=Pushetta(API_KEY)
-p.pushMessage(CHANNEL_NAME, "http://192.168.1.106:8081/")
+p.pushMessage(CHANNEL_NAME, "http://raspberrypi.local:8081/")
